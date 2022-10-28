505e61ae13051d8e4500aae3a9a83867a305e013
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-10-29 01:22:02 +0600

    bug fix

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { axios } from "axios";
+import axios from 'axios';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import './App.css';
 
@@ -10,79 +10,75 @@ function Home() {
   );
 }
 
-const PostDetail = (props) => {
-
-  console.log('----------------');
-  console.log(props);
-  console.log('----------------');
+const PostDetail = (data) => {
   return (
     <>
-      <h4>Title: {props.post.title}</h4>
-      <h6>Images: {props.post.images}</h6>
-      <h6>Em-Test: {props.post.emtest}</h6>
-      <h6>Article: {props.post.articleText}</h6>
+      {/* <h4>Title: {props.title}</h4>
+      <h6>Images: {props.imgUrl}</h6>
+      <h6>Em-Test: {props.emTest}</h6>
+      <h6>Article: {props.articleText}</h6> */}
+
+      <div align="left" className="col-md-10 blogShort" id={data.post.id}>
+        <h3>{data.post.title}</h3>
+        <img src={data.post.imgUrl} style={{ height: "50px", width: "50px" }} alt="post img" className="pull-left thumb margin10 img-thumbnail"></img>
+        <p>{data.post.emTest}</p>
+        <p>{data.post.articleText}</p>
+        {/* <Link to={location => `/post-detail/${post.id}`}>Detail</Link> &nbsp;
+        <Link to={location => `/post-edit/${post.id}`}>Edit</Link> &nbsp;
+        <Link to={location => `/post-delete/${post.id}`}>Delete</Link> &nbsp; */}
+      </div>
+
       <br></br>
     </>
   )
 };
 
 const Posts = () => {
-
-  // let [posts, usePosts] = useState([{
-  //   id: '1',
-  //   title: "Hello 1",
-  //   images: "images 1",
-  //   emtest: "Em Test value 1",
-  //   articleText: "Article Text value 1",
-  // },{
-  //   id: '2',
-  //   title: "Hello 2",
-  //   images: "images 2",
-  //   emtest: "Em Test value",
-  //   articleText: "Article Text value",
-  // },{
-  //   id: '3',
-  //   title: "Hello 3",
-  //   images: "images 3",
-  //   emtest: "Em Test value",
-  //   articleText: "Article Text value",
-  // }])
-
-  let [posts, usePosts] = useState()
+  let [posts, setPosts] = useState([{
+    id: '1',
+    title: "Hello 1",
+    images: "images 1",
+    emtest: "Em Test value 1",
+    articleText: "Article Text value 1",
+  }, {
+    id: '2',
+    title: "Hello 2",
+    images: "images 2",
+    emtest: "Em Test value",
+    articleText: "Article Text value",
+  }, {
+    id: '3',
+    title: "Hello 3",
+    images: "images 3",
+    emtest: "Em Test value",
+    articleText: "Article Text value",
+  }])
 
   useEffect(async () => {
-    const result = await axios(
+    const response = await axios(
       'http://127.0.0.1:5000/api/posts',
     );
-
-    console.log(result);
-
-    usePosts(result.data);
+    console.log(response.data);
+    setPosts(response.data.posts);
   }, []);
 
   let [count, setCount] = useState(100);
-
-  // const rows =[];
-  // for (let i = 0; i < 5; i++) {
-  //   rows.push(<PostDetail posts={posts} />)
-  // }
 
   return (
     <div className="container">
       <div id="blog" className="row">
         <div className="col-md-12 gap10">
-          {/* <PostDetail post={post} /> */}
+
           {/* -----------Increment && drecrement value-------- */}
           <button className='btn btn-primary' onClick={() => setCount(count - 1)}> -- </button> |.
           <button className='btn btn-info' onClick={() => setCount(count + 1)}> ++</button >
           <p>Default value 100, if you are click so vale will change: <h1>{count}</h1> </p>
           <p>----------------------------------------</p>
 
-         
+
           {
-            posts.map( p=> <PostDetail post={p} key={p.id} />)
+            posts.map(p => <PostDetail post={p} key={p.id} />)
           }
-         
         </div>
       </div>
     </div>
