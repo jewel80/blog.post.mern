dccccd8af61facbfdac4e6101746eaaaf86f2929
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-10-29 21:06:42 +0600

    redux added......
    redux added......

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,139 +1,57 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useForm } from "react-hook-form";
-import { BrowserRouter as Router, Link, Route, Switch, useParams } from "react-router-dom";
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams, useHistory } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { useSelector, useDispatch } from 'react-redux';
 
+const Home = () => { return (<h2>Hello. You are in Home</h2>) };
 
-function Home() {
-  return (
-    <h5>Welcome to our Home page.</h5>
-  );
-}
-
-const PostSummary = (data) => {
-  return (
-    <>
-      <div align="left" className="col-md-10 blogShort" id={data.post.id}>
-        <h3>{data.post.title}</h3>
-        <img src={data.post.imgUrl} style={{ height: "50px", width: "50px" }} alt="post img" className="pull-left thumb margin10 img-thumbnail"></img>
-        <p>{data.post.emTest}</p>
-        <Link to={location => `/post-detail/${data.post._id}`}>Detail</Link> &nbsp;
-        <Link to={location => `/post-edit/${data.post._id}`}>Edit</Link> &nbsp;
-        <Link to={location => `/post-delete/${data.post._id}`}>Delete</Link> &nbsp;
-      </div>
-      <br></br>
-    </>
-  )
-};
-
-const PostDetails = (data) => {
-  let {id} = useParams();
-  
-  useEffect(async () => {
-    const response = await axios(
-      `http://127.0.0.1:5000/api/posts/${id}`,
-      );
-      setPost(response.data.post);
-    }, [id]);
-    
-    let [post, setPost] = useState({});
-
-  return (
-    <>
-      <div align="left" className="col-md-10 blogShort" id={post._id}>
-        <h3>{post.title}</h3>
-        <img src={post.imgUrl} style={{ height: "50px", width: "50px" }} alt="post img" className="pull-left thumb margin10 img-thumbnail"></img>
-        <p>{post.emTest}</p>
-        <p>{post.articleText}</p>
-        <a className="btn btn-blog pull-right marginBottom10" href={post.readMoreUrl}>READ MORE</a>  &nbsp;
-        {/* <Link to={location => `/post-detail/${post.id}`}>Detail</Link> &nbsp; */}
-        <Link to={location => `/post-edit/${post._id}`}>Edit</Link> &nbsp;
-        <Link to={location => `/post-delete/${post._id}`}>Delete</Link> &nbsp;
-      </div>
-
-      <br></br>
-    </>
-  )
-};
-
-const Posts = () => {
-  
-  let [posts, setPosts] = useState([{
-    id: '1',
-    title: "Hello 1",
-    images: "images 1",
-    emtest: "Em Test value 1",
-    articleText: "Article Text value 1",
-  }, {
-    id: '2',
-    title: "Hello 2",
-    images: "images 2",
-    emtest: "Em Test value",
-    articleText: "Article Text value",
-  }, {
-    id: '3',
-    title: "Hello 3",
-    images: "images 3",
-    emtest: "Em Test value",
-    articleText: "Article Text value",
-  }])
-
-  useEffect(async () => {
-    const response = await axios(
-      'http://127.0.0.1:5000/api/posts',
-    );
-    setPosts(response.data.posts);
-  }, []);
-
-  let [count, setCount] = useState(100);
-
-  return (
-    <div className="container">
-      <div id="blog" className="row">
-        <div className="col-md-12 gap10">
-
-          {/* -----------Increment && drecrement value-------- */}
-          <button className='btn btn-primary' onClick={() => setCount(count - 1)}> -- </button> |.
-          <button className='btn btn-info' onClick={() => setCount(count + 1)}> ++</button >
-          <p>Default value 100, if you are click so vale will change: <h1>{count}</h1> </p>
-          <p>----------------------------------------</p>
-
-
-          {
-            posts.map(p => <PostSummary post={p} key={p.id} />)
-          }
-        </div>
-      </div>
-    </div>
-  )
-}
-
-function PostCreate() {
+const PostCreate = () => {
+  let history = useHistory();
+  let dispatch = useDispatch();
   const { register, handleSubmit, watch, errors } = useForm();
-  
-  
-  let saveData = async (data) => {
-    const response = await axios.post('http://127.0.0.1:5000/api/posts', data)
+
+  const [location, setLocation] = useState({});
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    //location = { latitude, longitude };  
+    setLocation({ latitude, longitude });
+  }
+
+  function error() {
+    console.error('Unable to retrieve your location');
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  let saveData = (data) => {
+    dispatch({
+      type: "ADD_POST", payload: data
+    })
+
+    history.push('/posts');
   }
 
   const onSubmit = data => {
-    saveData(data);
-  }
-
+    let payload = { ...data, lat: location.latitude.toString(), long: location.longitude.toString() };
+    saveData(payload);
+  };
 
   return (
     <>
       <h2>Create new Post</h2>
-      {/* <h4>Location: {location.latitude}, {location.longitude}</h4> */}
+      <h4>Location: {location.latitude}, {location.longitude}</h4>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group input-group">
-          <input type="text" placeholder="Title" name="title" ref={register({ required: true, maxLength: 100 })} className="form-control" />
+          <input type="text" placeholder="Title" name="title" ref={register({ required: true })} className="form-control" />
           <span>{errors.title && 'Title is required'}</span>
         </div>
         <div className="form-group input-group">
-          <input type="text" placeholder="Summary" name="emTest" ref={register({ required: true, maxLength: 100 })} className="form-control" />
+          <input type="text" placeholder="Summary" name="emText" ref={register({ required: true, maxLength: 100 })} className="form-control" />
         </div>
         <div className="form-group input-group">
           <textarea name="articleText" ref={register({ required: true })} className="form-control" />
@@ -145,26 +63,47 @@ function PostCreate() {
       </form>
     </>
   )
-}
+};
 
 const PostEdit = (props) => {
+  let history = useHistory();
   const { register, handleSubmit, watch, errors } = useForm();
   let { id } = useParams();
+  let dispatch = useDispatch();
 
+  const [location, setLocation] = useState({});
 
-  useEffect(async () => {
-    const response = await axios(
-      `http://127.0.0.1:5000/api/posts/${id}`,
-      );
-      setPost(response.data.post);
-    }, [id]);
-    
-    let [post, setPost] = useState({});
- 
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    //location = { latitude, longitude };  
+    setLocation({ latitude, longitude });
+  }
 
-  let updateData =async (data) => {
-    console.log(data);
-    const response = await axios.put(`http://127.0.0.1:5000/api/posts`, data)
+  function error() {
+    console.error('Unable to retrieve your location');
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  let fetchData = (id) => {
+    dispatch({ type: "FETCH_POST_DETAIL", payload: id });
+  }
+
+  useEffect(() => {
+    fetchData(id);
+  }, [id])
+
+  const post = useSelector(state => {
+    return state.posts.selectedPost;
+  });
+
+  let updateData = (data) => {
+    const payload = { ...data, id, lat: location.latitude.toString(), long: location.longitude.toString() };
+    dispatch({ type: "EDIT_POST", payload: payload });
+    history.push('/posts');
   }
 
   const onSubmit = data => {
@@ -174,16 +113,14 @@ const PostEdit = (props) => {
   return (
     <>
       <h2>Update new Post</h2>
+      <h4>Location: {location.latitude}, {location.longitude}</h4>
       <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-group input-group">
-          <input type="hidden"  name="id" defaultValue={post._id} ref={register({ required: true })} className="form-control" />
-        </div>
         <div className="form-group input-group">
           <input type="text" placeholder="Title" name="title" defaultValue={post.title} ref={register({ required: true })} className="form-control" />
           <span>{errors.title && 'Title is required'}</span>
         </div>
         <div className="form-group input-group">
-          <input type="text" placeholder="Summary" name="emTest" defaultValue={post.emTest} ref={register({ required: true, maxLength: 100 })} className="form-control" />
+          <input type="text" placeholder="Summary" name="emText" defaultValue={post.emText} ref={register({ required: true, maxLength: 100 })} className="form-control" />
         </div>
         <div className="form-group input-group">
           <textarea name="articleText" defaultValue={post.articleText} ref={register({ required: true })} className="form-control" />
@@ -201,29 +138,223 @@ const PostEdit = (props) => {
 const PostDelete = (props) => {
   const { register, handleSubmit, watch, errors } = useForm();
   let { id } = useParams();
+  let history = useHistory();
+  let dispatch = useDispatch();
 
-  useEffect(async () => {
-    const response = await axios.delete(
-      `http://127.0.0.1:5000/api/posts/${id}`,
-      );
-      setPost(response.data.post);
-    }, [id]);
-    
-    let [post, setPost] = useState({});
+  let fetchData = (id) => {
+    dispatch({ type: "FETCH_POST_DETAIL", payload: id });
+  }
+
+  useEffect(() => {
+    fetchData(id);
+  }, [id])
+
+  const post = useSelector(state => {
+    return state.posts.selectedPost;
+  });
+
+
+  let deleteData = () => {
+    dispatch({ type: "DELETE_POST", payload: id });
+    history.push('/posts');
+  }
+
+  const onSubmit = data => {
+    deleteData();
+  };
 
   return (
     <>
       <h2>Delete Post</h2>
-      {/* <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h2>{post.title}</h2>
         <img src={post.imgUrl} style={{ height: "50px", width: "50px" }} alt="post img" className="pull-left thumb margin10 img-thumbnail"></img>
         <article><p>{post.articleText}</p></article>
         <input type="submit" className="btn btn-primary btn-block" value="Delete" />
-      </form> */}
+      </form>
     </>
   )
 };
 
+const PostSummary = (post) => {
+  return (
+    <div align="left" className="col-md-10 blogShort" id={post.id}>
+      <h3>{post.title}</h3>
+      <img src={post.imgUrl} style={{ height: "50px", width: "50px" }} alt="post img" className="pull-left thumb margin10 img-thumbnail"></img>
+      <p>{post.emText}</p>
+      <Link to={location => `/post-detail/${post.id}`}>Detail</Link> &nbsp;
+      <Link to={location => `/post-edit/${post.id}`}>Edit</Link> &nbsp;
+      <Link to={location => `/post-delete/${post.id}`}>Delete</Link> &nbsp;
+    </div>
+  )
+}
+
+const Comments = (props) => {
+
+  let { id } = useParams();
+  let dispatch = useDispatch();
+
+  const comments = useSelector(state => {
+    return state.posts.selectedComments;
+  });
+
+  let fetchData = () => {
+    dispatch({
+      type: "FETCH_COMMENTS", payload: id,
+    })
+  }
+
+  useEffect(() => {
+    fetchData(id);
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">Comments count: {comments.length}</div>
+          <div className="col-sm-12">
+            {
+              comments.map(comment => {
+                let date = comment.date;
+                if (date) {
+                  let d = new Date(date.toString());
+                  date = d.toDateString();
+                }
+                else date = 'No Date';
+
+                return (
+                  <>
+                    <div className="panel panel-default" key={comment.id}>
+                      <div className="panel-body">
+                        {comment.body} [{date}]
+                      </div>
+                    </div>
+                  </>
+                );
+              }
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  )
+};
+
+const CommentCreate = () => {
+  let history = useHistory();
+  let dispatch = useDispatch();
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  let { id } = useParams();
+
+  let saveData = (data) => {
+
+    let comment = { body: data.title, date: new Date() };
+
+    dispatch({
+      type: "ADD_COMMENT", payload: { postId: id, data: comment }
+    })
+
+    history.goBack();
+  }
+
+
+
+  const onSubmit = data => {
+    saveData(data);
+  };
+
+  return (
+    <>
+      <div className="row">
+
+        <div className="col-md-6">
+          <div className="widget-area no-padding blank">
+            <div className="status-upload">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Write your comment here" name="title" ref={register({ required: true })} className="form-control" />
+                <input type="submit" className="btn btn-primary btn-block" />
+              </form>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </>
+  )
+};
+
+const PostDetail = (props) => {
+
+  let { id } = useParams();
+
+  let dispatch = useDispatch();
+
+  let fetchData = (id) => {
+    dispatch({ type: "FETCH_POST_DETAIL", payload: id });
+  }
+
+  useEffect(() => {
+    fetchData(id);
+  }, [])
+
+  const post = useSelector(state => {
+    return state.posts.selectedPost;
+  });
+
+  let match = useRouteMatch();
+
+  return (
+    <>
+      <div align="left" className="col-md-10 blogShort" id={id}>
+        <h2>{post.title}</h2>
+        <img src={post.imgUrl} style={{ height: "50px", width: "50px" }} alt="post img" className="pull-left thumb margin10 img-thumbnail"></img>
+        <article><p>{post.articleText}</p></article>
+        <a className="btn btn-blog pull-right marginBottom10" href={post.readMoreUrl}>READ MORE</a>
+      </div>
+      <div>
+        <h4>Comments</h4>
+        <Link className="btn btn-blog pull-right" to={`${match.url}/comments`}>Show comments</Link> &nbsp;
+        <Link className="btn btn-blog pull-right" to={`${match.url}/add-comment`}>Add comment</Link>
+        <Switch>
+          <Route path={`${match.path}/comments`} component={Comments}></Route>
+          <Route path={`${match.path}/add-comment`} component={CommentCreate}></Route>
+        </Switch>
+      </div>
+    </>
+  )
+};
+
+const Posts = () => {
+  let dispatch = useDispatch();
+
+  const posts = useSelector(state => {
+    return state.posts.postList;
+  });
+
+  let fetchData = () => {
+    dispatch({ type: "FETCH_POSTS" });
+    dispatch({ type: "CLEAR_SELECTION" });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div id="blog" className="row">
+        {
+          posts.map(p => <PostSummary {...p} key={p.id} />)
+        }
+        <div className="col-md-12 gap10"></div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -241,11 +372,12 @@ function App() {
 
           <div id="page-content-wrapper">
             <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-              <h3>Blog Website</h3>
+              <h3>hello world</h3>
             </nav>
+
             <div className="container-fluid">
               <Switch>
-                <Route path="/post-detail/:id"><PostDetails /></Route>
+                <Route path="/post-detail/:id"><PostDetail /></Route>
                 <Route path="/post-create"><PostCreate /></Route>
                 <Route path="/post-edit/:id"><PostEdit /></Route>
                 <Route path="/post-delete/:id"><PostDelete /></Route>
